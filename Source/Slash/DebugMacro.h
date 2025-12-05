d13d1840edfc7c64acf49cff6eeb79bbe7c58a4c
rinnli_o47 <witherlzero@outlook.com>
2025-12-05 21:33:03 +0800

    Add Tick Version for DrawDebug

--- Source/Slash/DebugMacro.h
+++ Source/Slash/DebugMacro.h
@@ -1,12 +1,22 @@
 ﻿#pragma once
 #include "DrawDebugHelpers.h" // Header files include draw debug functions
 
-
-#define DRAW_SPHERE(Location) if(GetWorld()) DrawDebugSphere(GetWorld(),Location,25.f,12,FColor::Red,true)  
+// Persistant Draw Version
+#define DRAW_SPHERE(Location) if(GetWorld()) DrawDebugSphere(GetWorld(),Location,25.f,12,FColor::Red,true);
 #define DRAW_LINE(StartLocation,EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(),StartLocation,EndLocation,FColor::Red,true,-1.f,0,1.f)
 #define DRAW_POINT(Location) if(GetWorld()) DrawDebugPoint(GetWorld(),Location,10.f,FColor::Red,true)
 #define DRAW_VECTOR(StartLocation,EndLocation) if(GetWorld()) \
 	{ \
 		DrawDebugLine(GetWorld(),StartLocation,EndLocation,FColor::Red,true,-1.f,0,1.f); \
 		DrawDebugPoint(GetWorld(),EndLocation,10.f,FColor::Red,true); \
+	}
+
+// Draw Version for Tick
+#define DRAW_LINE_PerFrame(StartLocation,EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(),StartLocation,EndLocation,FColor::Red,false,-1.f,0,1.f)
+#define DRAW_SPHERE_PerFrame(Location) if(GetWorld()) DrawDebugSphere(GetWorld(),Location,25.f,12,FColor::Red,false, -1.f);
+#define DRAW_POINT_PerFrame(Location) if(GetWorld()) DrawDebugPoint(GetWorld(),Location,10.f,FColor::Red,false, -1.f)
+#define DRAW_VECTOR_PerFrame(StartLocation,EndLocation) if(GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(),StartLocation,EndLocation,FColor::Red,false,-1.f,0,1.f); \
+		DrawDebugPoint(GetWorld(),EndLocation,10.f,FColor::Red,false, -1.f); \
 	}
