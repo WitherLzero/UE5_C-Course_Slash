bb6244fe8f20e6ab6271175cbd88db7017c10f36
rinnli_o47 <witherlzero@outlook.com>
2025-12-05 14:59:50 +0800

    Draw Debug Lines

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -19,39 +19,26 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-	
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1,60.f,FColor::Cyan,TEXT("Item OnScreen Message!"));
-	}
-	
 	UWorld* World = GetWorld(); // Get the world pointer that this actor is in
-
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	
 	if (World)
 	{
-		FVector Location = GetActorLocation();
-		DrawDebugSphere(World,Location,30.f,24,FColor::Orange,false,30.f);
 		
-		// use macro to draw sphere
-		DRAW_SPHERE(Location);
-	}
+		DrawDebugLine(World, Location, Location + Forward * 200.f, FColor::Orange, true); 
 
+	}
+	
+	// use macro to draw
+	DRAW_SPHERE(Location);
+	DRAW_LINE(Location, Location + Forward * 100.f);
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
-	UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"),DeltaTime);
-	
-	FString Name = GetName();
-	FString Message = FString::Printf(TEXT("Item Name: %s"),*Name);  // use * to get the c-style array
-
-	if(GEngine){
-		GEngine->AddOnScreenDebugMessage(2,60.f,FColor::Blue,Message);
-	}
 
 }
 
