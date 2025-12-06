ef30bd6c2fe460c236b1e24fe17c8e782ff9d915
rinnli_o47 <witherlzero@outlook.com>
2025-12-06 23:42:52 +0800

    Create the bird pawn class with a capsule

---
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -0,0 +1,31 @@
+﻿// copyright: none
+
+
+#include "Pawns/Bird.h"
+
+
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	SetRootComponent(Capsule);
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
