891f37025bb94b1721d21bffd9d5ece2388034f9
rinnli_o47 <witherlzero@outlook.com>
2025-12-06 11:45:43 +0800

    Expose Variables to the Event Graph

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -1,5 +1,4 @@
 // copyright: none
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -20,18 +19,16 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-private:
-	// UPROPERTY(VisibleDefaultsOnly): can only see variables in blueprints
-	// UPROPERTY(VisibleInstanceOnly): can only see variables on instances
-	UPROPERTY(VisibleAnywhere) // can only see variables both in blueprints and on instances
-	float RunningTime;
-	
-	// UPROPERTY(EditDefaultsOnly)： can see and edit variables in blueprints
-	// UPROPERTY(EditInstanceOnly)： can see and edit variables on instances
-	UPROPERTY(EditAnywhere) // can see and edit variables both in blueprints and on instances
-	float Amplitude;
-	UPROPERTY(EditAnywhere)
-	float TimeConstant;
 
+	// BlueprintReadOnly: can only get variables in event graph
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Paramaters") 
+	float Amplitude;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Paramaters")
+	float TimeConstant;
+private:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,meta = (AllowPrivateAccess = "true")) 
+	float RunningTime;
 };
+
+
